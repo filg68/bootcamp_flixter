a29fa8da301dd7e5d775c9bb607648b8818b40b8
Fil <runfilrun@gmail.com>
2019-03-19 15:53:14 +0000

    Added instructor authentication code to the instructor\courses controller.  Generates an error when trying to create a new course as it is looking for a course id but one does not exist.  Have commented out the before_action check to avoid error but app not secure.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,6 +6,8 @@ class Instructor::CoursesController < ApplicationController
     @course = Course.new
   end
 
+
+
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
@@ -20,11 +22,11 @@ class Instructor::CoursesController < ApplicationController
 
   private
 
-  #def require_authorized_for_current_course
-  #  if @course.user != current_user
-  #    render plain: "Unauthorized", status: :unauthorized
-  #  end
-  #end
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_course
   def current_course
