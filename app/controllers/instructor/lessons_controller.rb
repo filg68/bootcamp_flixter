0d5e63bcacfb4713e592c99be565189f1c1ff0e8
Fil <runfilrun@gmail.com>
2019-03-23 13:56:04 +0000

    Retrofitted instructor lesson new view as it was not completed.

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,12 +3,14 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section
 
   def new
+    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @lesson = current_section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(current_section.course)
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(@section.course)
   end
 
   private
