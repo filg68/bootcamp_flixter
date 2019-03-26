27dab714df5e9e811345cefe152ad23bdbe4a723
Fil <runfilrun@gmail.com>
2019-03-26 14:16:43 +0000

    Added verification for user enrollment to display lesson.

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -26,7 +26,6 @@ class Instructor::LessonsController < ApplicationController
     @current_section ||= Section.find(params[:section_id])
   end
 
-
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video)
   end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,8 @@
 class LessonsController < ApplicationController
   # Lessons controller for non instructor views
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
 
@@ -8,6 +11,14 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_lesson
+    course = current_lesson.section.course
+    if current_user.enrolled_in?(course) == false 
+      redirect_to course_path(course[:id]), alert: 'You must enroll for this course to view lessons.'
+    end
+
   end
 
 end
